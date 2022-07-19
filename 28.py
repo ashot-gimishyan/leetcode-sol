2f5c8aff67514f0581a2e2bdc98485a5c44275b6
Ashot Gimishyan <gimishyan.aa@phystech.edu>
2022-07-19 10:42:03 +0300

    Create 28.py

---
+++ 28.py
@@ -0,0 +1,11 @@
+import re
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        
+        return haystack.find(needle)
